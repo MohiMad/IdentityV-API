d50e44b0ec5c5980e389adb55f94c9b0ab0d7bb7
Mohi <mohamad.kanjo@skola.uppsala.se>
2021-10-18 18:29:51 +0200

    Added new functions
    1- formatName function to parse the name of the skin as output
    
    2- formatQueryName which parses the `name` property to match the fandom's requirements

--- src/js/static.js
+++ src/js/static.js
@@ -1,12 +1,18 @@
 const params = require("../json/params.json");
 
 module.exports = {
-    errorStatus: () => {
-        return JSON.parse(`{"status": 404}`);
+    errorStatus: (msg = "Page not found!") => {
+        return JSON.parse(`{"status": 404, "message": "${msg}"}`);
     },
     emptyQuery: (query) => {
         return !params.some(param => {
             return Object.keys(query).includes(param);
         });
+    },
+    formatName: (name) => {
+        return name.replace("_", " ");
+    },
+    formatQueryName: (name) => {
+        return name.split(/\%20|\s/g).map(elm => elm.charAt(0).toUpperCase() + elm.slice(1)).join("_")
     }
 }
