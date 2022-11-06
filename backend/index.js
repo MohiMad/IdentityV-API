cb67bc571f697c3b2889e1f2e5c23ea34a4b6f61
Mohi <mohamad.kinjo@gmail.com>
2022-11-06 19:56:03 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ staticData.redirections.forEach(({ route, redirectTo }) => {
 app.get("/version", (req, res) => res.json({ version: Utility.getVersion() }));
 
 app.get("/*", (req, res) => {
-    res.render("index.html");
+    res.render("./index.html");
 }); 
 
 app.listen(process.env.PORT || 3000);
